7455a8a3729ccd706fead500e206b6d96e53bb4b
Julian Matias Garate <jgarate89@gmail.com>
2016-06-01 20:27:04 -0300

    Mando con el encoding cambiado desde el IDE, si no anda lo cambio por consola.

--- test/fiuba/algo3/AlgoFormerstest.java
+++ test/fiuba/algo3/AlgoFormerstest.java
@@ -1,11 +1,13 @@
 package fiuba.algo3.tests;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import fiuba.algo3.modelo.*;
+
 import org.junit.Assert;
 import org.junit.Test;
-
 
 public class AlgoFormerstest {
 
@@ -19,11 +21,11 @@ public class AlgoFormerstest {
 
 		Partida partida = new Partida(jugador1, jugador2);
 
-		// En el caso que no hubiese ningún AF devuelve:
-		// "Casillero vacío"
+		// En el caso que no hubiese ningï¿½n AF devuelve:
+		// "Casillero vacï¿½o"
 		nombre1 = partida.obtenerAlgoformer("A1").obtenerNombre();
 
-		// estos movimientos los tomaríamos por los clicks en el tablero
+		// estos movimientos los tomarï¿½amos por los clicks en el tablero
 		// Este caso mueve el AlgoF. de A1 a A3. (click en A1, A2 y A3)
 		movimientos.add("A1");
 		movimientos.add("A2");
