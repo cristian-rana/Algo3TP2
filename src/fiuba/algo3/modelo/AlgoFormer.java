6955fe0b5f5707498bc49e04584f67ff36e296e7
Diego <diego.alfonso2@gmail.com>
2016-06-02 00:15:44 -0300

    Pruebas unitarias de transformacion de AlgoFormer

--- src/fiuba/algo3/modelo/AlgoFormer.java
+++ src/fiuba/algo3/modelo/AlgoFormer.java
@@ -7,31 +7,48 @@ public class AlgoFormer {
 	
 	private String nombre;
 	private int puntosDeVida;
-	private int ataque;
-	private int distAtaque;
-	private int velocidad;
-	private List<Modo> modos;
-	
+	private Modo modoActivo;
+	private Modo modoInactivo;
 
-	public AlgoFormer() {
-				
+	public AlgoFormer(String nombre, int puntosDeVida, Modo modoHumanoide, Modo modoAlterno) {
+		this.nombre = nombre;
+		this.puntosDeVida = puntosDeVida;
+		this.modoActivo = modoHumanoide;
+		this.modoInactivo = modoAlterno;
 	}
 	
-	public boolean ataqueValido() {
-		return false;
-	}
-	
-	public boolean movimientoValido() {
-		return false;
-	}
-	
-	public String obtenerNombre() {
+	public String getNombre() {
 		return this.nombre;
 	}
-
 	
+	public String getAvatar() {
+		return modoActivo.getAvatar();
+	}
 
-
-
+	public int getPuntosDeVida() {
+		return puntosDeVida;
+	}
+	
+	public int getPtosDeAtaque() {
+		return modoActivo.getAtaque();
+	}
+	
+	public int getDistanciaAtaque() {
+		return modoActivo.getDistAtaque();
+	}
+	
+	public int getVelocidad() {
+		return modoActivo.getVelocidad();
+	}
+	
+	public void cambiarModo() {
+		Modo tmp = modoActivo;
+		modoActivo = modoInactivo;
+		modoInactivo = tmp;
+	}
+	
+	public boolean estaVivo() {
+		return puntosDeVida > 0;
+	}
 }
 
