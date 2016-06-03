2e411a15bf8b0564db4d8786dcdeb9064cf92a44
Julian Matias Garate <jgarate89@gmail.com>
2016-06-03 17:54:11 -0300

    Primera entrega

--- src/fiuba/algo3/modelo/Alterno.java
+++
@@ -1,9 +0,0 @@
-package fiuba.algo3.modelo;
-
-/**
- * Created by Julian Garate on 6/1/16.
- */
-public class Alterno implements Modo{
-
-
-}
--- src/fiuba/algo3/modelo/Humanoide.java
+++
@@ -1,7 +0,0 @@
-package fiuba.algo3.modelo;
-
-/**
- * Created by Julian Garate on 6/1/16.
- */
-public class Humanoide implements Modo{
-}
